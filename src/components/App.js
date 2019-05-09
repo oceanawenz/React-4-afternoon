38d980be8520f839763e1f12a9d6288cf3f18f6d
Oceana Wenz <oceana.wenz@gmail.com>
2019-05-09 15:44:53 -0700

    completed afternoon

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
@@ -13,38 +15,67 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(`practiceapi.devmountain.com/api/posts`).then(response => {
+      this.setState({ posts: response.data });
+    });
   }
 
-  updatePost() {
-  
+  //because Id and text of the post will be different every time the method is called, we should
+  //use an id and text parameter for the method
+  //use the parameters to construct our axios request and use the returned data to update posts on state.
+  //pass it down to the component as a new prop called updatePostFn
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text }).then(response => {
+      this.setState({ posts: response.data });
+    });
   }
 
-  deletePost() {
-
+  //the endpoint uses the request query to determin the id of the post to delete. 
+  //because the id of the post will be different every time, we pass id as a parameter for this method.
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`).then(response => {
+      this.setState({ posts: response.data });
+    })
   }
 
-  createPost() {
-
+  createPost(text) {
+    axios.post(`https://practiceapi.devmountain.com/api/posts`, { text }).then(response => {
+      this.setState({ posts: response.data });
+    })
   }
 
   render() {
     const { posts } = this.state;
+
 
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
+          {/* pass this.createPost to Compose method */}
+          <Compose createPostFn={this.createPost} />
+          {
+            //  map over the posts. render a Post component for every post in the posts array on state.
+            // pass props down into the Post Component in order to see the text and date for each post.
+            //then render text and date into the Post Component.
+            posts.map(post => (
+              <Post key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
 
-          <Compose />
-          
+              />
+            ))
+          }
         </section>
       </div>
     );
